8c76777560345def9e93789d1cb608649ec492d0
NaveedNaseem84 <naveed.naseem@nwas.nhs.uk>
2025-05-28 10:30:16 +0100

    add fixtures: Pizzas and Extras. Create deals. display raw data on menu page - retrieve ok

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from .models import Deal, Pizza, Extras
 
 # Create your views here.
 
-
 def menu_view(request):
+    
+    deal = Deal.objects.filter(active='Yes')
+    pizza = Pizza.objects.filter(active="Yes")
+    extra = Extras.objects.filter(active='Yes')  
 
-    return render(request, 'menu/menu.html')
+    template = "menu/menu.html"
+    context = {
+        "deals": deal,
+        "pizza": pizza,
+        "extras": extra
+    }
+
+    return render(request, template, context)
     
