accc40f98b099507d1c847360f7f85e2e484be0b
NaveedNaseem84 <naveed.naseem@nwas.nhs.uk>
2025-05-28 17:53:09 +0100

    create product detail view and menu detail template

--- menu/urls.py
+++ menu/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.menu_view, name='menu'),
-    
+    path('product_detail/<product_id>', views.product_detail, name='product_detail'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Deal, Pizza, Extras
 
 # Create your views here.
 
+
 def menu_view(request):
-    
+
     deal = Deal.objects.filter(active='Yes')
     pizza = Pizza.objects.filter(active="Yes")
-    extra = Extras.objects.filter(active='Yes')  
+    extra = Extras.objects.filter(active='Yes')
 
     template = "menu/menu.html"
     context = {
@@ -17,4 +18,26 @@ def menu_view(request):
     }
 
     return render(request, template, context)
-    
+
+
+def product_detail(request, product_id):
+    selected_product = None    
+    item= request.GET.get('item_type')
+
+    if item =="pizza":
+        selected_product = get_object_or_404(Pizza, pk=product_id)
+    elif item=="deal":
+        selected_product = get_object_or_404(Deal, pk=product_id)
+    elif item in ("side","drink","dessert"):
+         selected_product = get_object_or_404(Extras, pk=product_id)
+
+    print(selected_product.name)
+    print(selected_product.description)
+    print(selected_product.price)
+    context = {
+        "selected_product": selected_product,
+        "item": item
+    }
+
+    return render(request, 'menu/menu_detail.html', context)
+
