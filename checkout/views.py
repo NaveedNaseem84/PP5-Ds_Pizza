98bb64ab2d57567908f747d6f419bf2fb0400576
NaveedNaseem84 <naveed.naseem@nwas.nhs.uk>
2025-06-02 17:59:49 +0100

    add stripe elements and  JS and render card on template

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from.forms import orderForm
 
 # Create your views here.
 
 
 def checkout_view(request):
 
-    return render(request, 'checkout/checkout.html')
-    
+    order_form = orderForm(data=request.POST)
+    template = 'checkout/checkout.html'
+    context = {
+            'order_form': order_form,
+            'stripe_public_key': 'pk_test_51ROMmoRq53b8oxxiELQlxjLJj339AZZZM6nfxlfNtQcszTYsLYuqdgvicVNhBpgfEgnm7DoyZ72fE5ONEIzrvV4600L6rDAclB',
+            'client_secret': 'test_client_key',
+        }
+
+    return render(request, template, context)
